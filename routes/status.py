45fa302fe0060ae44fb46cfae30df4506ba579a9
Deepali123verma <166192241+Deepali123verma@users.noreply.github.com>
2026-01-31 23:23:18 +0530

    Update status.py

--- routes/status.py
+++ routes/status.py
@@ -1,1 +1,10 @@
+from flask import Blueprint, jsonify
+
+# Blueprint create karo
+bp = Blueprint("status", __name__)
+
+# /api/status endpoint
+@bp.route("/status")
+def status():
+    return jsonify({"status": "ok", "message": "Hotel Management API is running"})
 
